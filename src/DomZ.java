b84a1d40d95d311716e3ba3c33ec9ac16e733dc8
Dinar S <ka6aup@gmail.com>
2024-06-01 22:03:44 +0300

    !

--- src/DomZ.java
+++ src/DomZ.java
@@ -3,7 +3,7 @@ public class DomZ {
     public static void main(String[] args) {
         int x = 2;
         int y = 4;
-        int z = 6;
+        int z = 5;
         int d = 8;
         System.out.print(x + "   " + y + "   " + z + "   " + d);
     }
