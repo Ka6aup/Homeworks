b5299043478f16f58b9b5c40b3beb70cb15bc49d
Dinar S <ka6aup@gmail.com>
2024-06-05 21:34:17 +0300

    Дом задание номер 1 30.05

---
+++ src/ru/Innopolis/java/DomZadan30may.java
@@ -0,0 +1,9 @@
+package ru.Innopolis.java;
+
+public class DomZadan30may {
+    public static void main(String[] args) {
+        double farenheit = 212;
+        double celsius = (farenheit - 32) * 5/9;
+        System.out.println(farenheit + " градусов по Фаренгейту равна " + celsius + " по Цельсию");
+    }
+}
