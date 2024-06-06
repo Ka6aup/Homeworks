0bd38931f80f43a38775c1c80e0a8cd70a59ca07
Dinar S <ka6aup@gmail.com>
2024-06-06 16:59:06 +0300

    Дом задание номер 30.05

---
+++ src/ru/Innopolis/java/Dom3Zadan30may.java
@@ -0,0 +1,15 @@
+package ru.Innopolis.java;
+
+public class Dom3Zadan30may {
+    public static void main(String[] args) {
+
+        String st = "Java";
+        String re = st.repeat(8);
+        System.out.println(re);
+    }
+}
+
+
+
+
+
---
+++ src/ru/Innopolis/java/Dom4Zadan30may.java
@@ -0,0 +1,18 @@
+package ru.Innopolis.java;
+
+public class Dom4Zadan30may {
+    public static void main(String[] args) {
+
+        int stb = 10;
+        int str = 10;
+        char k = '-';
+
+        for (int i = 0; i < str; i++) {
+            for (int j = 0; j < stb; j++) {
+                System.out.print(k);
+
+            }
+            System.out.println();
+        }
+    }
+}
