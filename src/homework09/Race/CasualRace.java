2632edfe0750d0ad4036f3ba982c3fc541c75ed7
Dinar S <ka6aup@gmail.com>
2024-07-16 00:13:04 +0300

    Дом задание

---
+++ src/homework09/Race/CasualRace.java
@@ -0,0 +1,17 @@
+package homework09.Race;
+import java.util.StringJoiner;
+
+public class CasualRace extends Race {
+    public CasualRace() {
+    }
+
+    public CasualRace(String type, Integer distance, String route, Integer prizeFound) {
+        super(type, distance, route, prizeFound);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", CasualRace.class.getSimpleName() + "[", "]")
+                .toString();
+    }
+}
---
+++ src/homework09/Race/DragRace.java
@@ -0,0 +1,17 @@
+package homework09.Race;
+import java.util.StringJoiner;
+
+public class DragRace extends Race {
+    public DragRace() {
+    }
+
+    public DragRace(String type, Integer distance, String route, Integer prizeFound) {
+        super(type, distance, route, prizeFound);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", DragRace.class.getSimpleName() + "[", "]")
+                .toString();
+    }
+}
