ac91323d3d60252b9273b9cd28a47bd9b45ed1f1
Ka6aup <ka6aup@gmail.com>
2024-08-29 11:26:29 +0300

    Delete src/attestation/attestation01 directory

--- src/attestation/attestation01/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package attestation.attestation01.exceptions;
-
-public class UserNotFoundException extends IllegalArgumentException {
-    static final String MESSAGE = "Пользователя с заданным идентификатором не существует";
-    public UserNotFoundException() {
-        super(MESSAGE);
-    }
-}
--- src/attestation/attestation01/exceptions/WrongPasswordException.java
+++
@@ -1,8 +0,0 @@
-package attestation.attestation01.exceptions;
-
-public class WrongPasswordException extends IllegalArgumentException {
-    static final String MESSAGE = "Пароль имеет неверный формат!";
-    public WrongPasswordException() {
-        super(MESSAGE);
-    }
-}
