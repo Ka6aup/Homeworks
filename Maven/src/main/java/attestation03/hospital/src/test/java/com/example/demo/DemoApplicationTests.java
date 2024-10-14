fd5266e05ba4dac54de7930891a12d85ea7fa4d0
Dinar S <ka6aup@gmail.com>
2024-10-14 16:20:57 +0300

    Добавление тестов

---
+++ Maven/src/main/java/attestation03/hospital/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package attestation03.hospital.src.test.java.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
