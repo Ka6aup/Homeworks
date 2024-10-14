12ab8ad67c38f32da7acb56c2dcba0ed5200a993
Dinar S <ka6aup@gmail.com>
2024-10-14 18:10:50 +0300

    Hw16

---
+++ Maven/src/main/java/BonusService/Main.java
@@ -0,0 +1,16 @@
+package BonusService;
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+
+        //service.calculate(amount, registered);
+
+        //long bonus = service.calculate(amount, registered);
+        //System.out.println("Bonus amount: " + bonus);
+        System.out.println(service.calculate(amount, registered));
+
+    }
+}
